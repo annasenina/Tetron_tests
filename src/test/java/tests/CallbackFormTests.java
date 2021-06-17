6a5c3d8c93ea8d940ada5690fe2e1ba65bf75ac0
Anna Senina <as.senina@gmail.com>
2021-06-17 17:13:38 +0300

    Первый тест на открытие формы формы Заказать звонок

--- src/test/java/tests/CallbackFormTests.java
+++ src/test/java/tests/CallbackFormTests.java
@@ -1,20 +1,39 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.AllureId;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
+
+import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import static io.qameta.allure.Allure.step;
+
 
 public class CallbackFormTests extends TestBase{
 
     @Test
     @Tag("web")
-    void openCallbackFormTest()
+    @AllureId("3154")
+    @DisplayName("При нажатии на кнопку \"Заказать звонок\" открывается форма \"Заказать звонок\"")
+    void canOpenCallbackFormTest()
     {
+        step ("Открываем главную страницу", ()-> open(""));
 
-        open("");
-        //order_call
+        //$(".top_block").find(byText("Заказать звонок")).click();
+        step ("Нажимаем на кнопку \"Заказать звонок\"", ()-> $(byAttribute("data-popup","js_orerd_popup")).click());
 
+        step ("Открывается форма с заголовком \"Заказать звонок\"", ()-> {
+                    $$(".content_popup").first().shouldBe(visible);
+                    $$(".content_popup").first().shouldHave(text("Заказать звонок"));
+                });
+        step ("На форме есть поля \"Ваше имя\" и \"Телефон\"", ()-> {
+
+                    $$(".content_popup").first().shouldHave(text("Ваше имя"));
+                    $$(".content_popup").first().shouldHave(text("Телефон"));
+        });
         sleep(5000);
     }
 }
